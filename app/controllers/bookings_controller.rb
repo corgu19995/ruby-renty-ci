0f1665bc37e5663d6f70f3dade65a1f7297a5ef0
CEIBA\cesar.munoz <cesar.munoz@ceiba.com.co>
2018-12-14 00:04:40 -0500

    Se ajusta la linea porque saca error cuando se sube a heroku

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -139,9 +139,8 @@ class BookingsController < ApplicationController
                         .joins("INNER JOIN rentals on rentals.id=cars.rental_id")   
                         .joins("INNER JOIN type_vehicles on type_vehicles.id=cars.type_vehicle_id")
                         .where("bookings.user=:user and bookings.id=:bookingId",{user:localId,bookingId:params[:bookingId]})
-                        #.take
             
-            if @booking.blank?
+            if @booking.rows<=0
                 data = JSON.parse('{"message":"Reserva no encontrada"}')
                 json_response(data);  
             else
