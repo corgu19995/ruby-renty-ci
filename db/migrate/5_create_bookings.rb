e2d485d9df3626ca4c8aadc352d0948918aebbd6
CEIBA\cesar.munoz <cesar.munoz@ceiba.com.co>
2018-12-14 01:25:07 -0500

    Se realiza validaciones para probar en heroku

--- db/migrate/5_create_bookings.rb
+++ db/migrate/5_create_bookings.rb
@@ -3,11 +3,11 @@ class CreateBookings < ActiveRecord::Migration[5.1]
     create_table :bookings do |t|
       t.string :user
       t.references :car, foreign_key: true
-      t.datetime :bookingDate
+      t.datetime :bookingdate
       t.string :pickup
-      t.datetime :pickupDate
-      t.string :deliverPlace
-      t.datetime :deliverDate
+      t.datetime :pickupdate
+      t.string :deliverplace
+      t.datetime :deliverdate
     end
   end
 end
